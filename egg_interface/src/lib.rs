4d377a5102c08fad70741fbb4151f1d72d172b45
phannebohm <phannebohm@hsbi.de>
2023-05-09 16:19:36 +0200

    Add `egg` and `egg_interface` to 3rdParty
    - `egg` is a submodule linking to the original rust/egg
    
    - `egg_interface` has C-like functions that can be used inside
      MetaModelica with:
      `external "C" rust_f() annotation(Library="omcruntime");`

---
+++ egg_interface/src/lib.rs
@@ -0,0 +1,159 @@
+use std::ffi::{c_void, CStr};
+use std::os::raw::c_char;
+use std::slice;
+
+use egg::*;
+
+define_language! {
+    enum SimpleLanguage {
+        Num(i32),
+        "+" = Add([Id; 2]),
+        "-" = Sub([Id; 2]),
+        "*" = Mul([Id; 2]),
+        "/" = Div([Id; 2]),
+        "^" = Pow([Id; 2]),
+        "der" = Der(Id),
+        "sin" = Sin(Id),
+        Symbol(Symbol),
+    }
+}
+
+fn make_rules() -> Vec<Rewrite<SimpleLanguage, ()>> {
+    vec![
+        rewrite!("commute-add"; "(+ ?a ?b)" => "(+ ?b ?a)"),
+        rewrite!("commute-mul"; "(* ?a ?b)" => "(* ?b ?a)"),
+        rewrite!("add-0"; "(+ ?a 0)" => "?a"),
+        rewrite!("sub-inv"; "(- ?a ?a)" => "0"),
+        rewrite!("mul-0"; "(* ?a 0)" => "0"),
+        rewrite!("mul-1"; "(* ?a 1)" => "?a"),
+        rewrite!("sin-0"; "(sin 0)" => "0"),
+    ]
+}
+
+/// parse an expression, simplify it using egg, and pretty print it back out
+fn simplify(s: &str) -> String {
+    // parse the expression, the type annotation tells it which Language to use
+    let expr: RecExpr<SimpleLanguage> = s.parse().unwrap();
+
+    // simplify the expression using a Runner, which creates an e-graph with
+    // the given expression and runs the given rules over it
+    let runner = Runner::default().with_expr(&expr).run(&make_rules());
+
+    // the Runner knows which e-class the expression given with `with_expr` is in
+    let root = runner.roots[0];
+
+    // use an Extractor to pick the best element of the root eclass
+    let extractor = Extractor::new(&runner.egraph, AstSize);
+    let (best_cost, best) = extractor.find_best(root);
+    println!("Simplified {} to {} with cost {}", expr, best, best_cost);
+    best.to_string()
+}
+
+#[test]
+fn simple_tests() {
+    assert_eq!(simplify("(* 0 42)"), "0");
+    assert_eq!(simplify("(+ 0 (* 1 foo))"), "foo");
+}
+
+/* OMC INTERFACE */
+
+#[no_mangle]
+pub extern "C" fn rust_parse_equation(eq_str: *const c_char, comment: *const c_char) -> *mut c_char {
+    let s = unsafe { CStr::from_ptr(eq_str).to_string_lossy().into_owned() };
+    let cs = unsafe { CStr::from_ptr(comment).to_string_lossy().into_owned() };
+    //println!("Rust: {:?} = {:?}", cs, s);
+    //println!("  = {:?}", simplify(&s));
+    simplify(&s).as_ptr()
+}
+
+/* useful functions between Rust and C */
+
+/*
+// A Rust struct mapping the C struct
+#[repr(C)]
+#[derive(Debug)]
+pub struct RustStruct {
+    pub c: char,
+    pub ul: u64,
+    pub c_string: *const c_char,
+}
+
+macro_rules! create_function {
+    // This macro takes an argument of designator `ident` and
+    // creates a function named `$func_name`.
+    // The `ident` designator is used for variable/function names.
+    ($func_name:ident, $ctype:ty) => {
+        #[no_mangle]
+        pub extern "C" fn $func_name(v: $ctype) {
+            // The `stringify!` macro converts an `ident` into a string.
+            println!(
+                "{:?}() is called, value passed = <{:?}>",
+                stringify!($func_name),
+                v
+            );
+        }
+    };
+}
+
+// create simple functions where C type is exactly mapping a Rust type
+//create_function!(rust_char, char);
+//create_function!(rust_wchar, char);
+create_function!(rust_short, i16);
+create_function!(rust_ushort, u16);
+create_function!(rust_int, i32);
+create_function!(rust_uint, u32);
+create_function!(rust_long, i64);
+create_function!(rust_ulong, u64);
+create_function!(rust_void, *mut c_void);
+
+// for NULL-terminated C strings, it's a little bit clumsier
+#[no_mangle]
+pub extern "C" fn rust_string(c_string: *const c_char) {
+    // build a Rust string from C string
+    let s = unsafe { CStr::from_ptr(c_string).to_string_lossy().into_owned() };
+
+    println!("rust_string() is called, value passed = <{:?}>", s);
+}
+
+// for C arrays, need to pass array size
+#[no_mangle]
+pub extern "C" fn rust_int_array(c_array: *const i32, length: usize) {
+    // build a Rust array from array & length
+    let rust_array: &[i32] = unsafe { slice::from_raw_parts(c_array, length as usize) };
+    println!(
+        "rust_int_array() is called, value passed = <{:?}>",
+        rust_array
+    );
+}
+
+#[no_mangle]
+pub extern "C" fn rust_string_array(c_array: *const *const c_char, length: usize) {
+    // build a Rust array from array & length
+    let tmp_array: &[*const c_char] = unsafe { slice::from_raw_parts(c_array, length as usize) };
+
+    // convert each element to a Rust string
+    let rust_array: Vec<_> = tmp_array
+        .iter()
+        .map(|&v| unsafe { CStr::from_ptr(v).to_string_lossy().into_owned() })
+        .collect();
+
+    println!(
+        "rust_string_array() is called, value passed = <{:?}>",
+        rust_array
+    );
+}
+
+// for C structs, need to convert each individual Rust member if necessary
+#[no_mangle]
+pub unsafe extern "C" fn rust_cstruct(c_struct: *mut RustStruct) {
+    let rust_struct = &*c_struct;
+    let s = CStr::from_ptr(rust_struct.c_string)
+        .to_string_lossy()
+        .into_owned();
+
+    println!(
+        "rust_cstruct() is called, value passed = <{} {} {}>",
+        rust_struct.c, rust_struct.ul, s
+    );
+}
+*/
